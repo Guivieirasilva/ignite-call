5812e7f50f6df8ea618c62d6d65df89e97e134d5
gui vieira <guivieirasilva02@gmail.com>
2024-10-30 22:36:18 -0300

    rename function - ConnectCalendar

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -21,7 +21,7 @@ const registerFormSchema = z.object({
 
 type RegisterFormData = z.infer<typeof registerFormSchema>;
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession();
 
   const route = useRouter();
