079df1d9dde948262712387adb4b527dec61044f
gui vieira <guivieirasilva02@gmail.com>
2024-11-08 20:47:27 -0300

    chore: Visual structure of the timetable list

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from "../../../../../components/Calendar";
-import { Container } from "./styles";
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
 
-export function CalendarStep () {
+export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Terça-Feira <span>03 de Novembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>01:00</TimePickerItem>
+            <TimePickerItem>02:00</TimePickerItem>
+            <TimePickerItem>03:00</TimePickerItem>
+            <TimePickerItem>04:00</TimePickerItem>
+            <TimePickerItem>05:00</TimePickerItem>
+            <TimePickerItem>06:00</TimePickerItem>
+            <TimePickerItem>07:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
-  )
+  );
 }
