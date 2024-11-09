d924c6bcd69b328a1798b13a24d7707a8942d92a
gui vieira <guivieirasilva02@gmail.com>
2024-11-09 10:01:08 -0300

    chore: confirmation form

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,11 +1,32 @@
 import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
-import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
 import { CalendarBlank, Clock } from "phosphor-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const ConfirmFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "O nome precisa de no mínimo 3 caracteres" }),
+  email: z.string().email({ message: "Digite um e-mail válido" }),
+  observations: z.string().nullable(),
+});
+
+type ConfirmFormData = z.infer<typeof ConfirmFormSchema>;
 
 export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(ConfirmFormSchema),
+  });
+
   function handleConfimScheduling() {}
   return (
-    <ConfirmForm as="form" onSubmit={handleConfimScheduling}>
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfimScheduling)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
@@ -23,7 +44,9 @@ export function ConfirmStep() {
           crossOrigin={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
+          {...register("name")}
         />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
       </label>
 
       <label>
@@ -34,19 +57,25 @@ export function ConfirmStep() {
           crossOrigin={undefined}
           onPointerEnterCapture={undefined}
           onPointerLeaveCapture={undefined}
+          {...register("email")}
         />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
       </label>
 
       <label>
         <Text size="sm">Observações</Text>
-        <TextArea />
+        <TextArea {...register("observations")} />
       </label>
 
       <FormActions>
         <Button type="button" variant="tertiary">
           Cancelar
         </Button>
-        <Button type="submit">Confirmar</Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
       </FormActions>
     </ConfirmForm>
   );
