a02c39816b6741ec042e2e77d5cb53dd2c1c186f
gui vieira <guivieirasilva02@gmail.com>
2024-11-13 19:34:40 -0300

    chore: Scheduling table

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -7,11 +7,15 @@ import {
   TimePickerItem,
   TimePickerList,
 } from "./styles";
+import dayjs from "dayjs";
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  
+
   const isDateSelected = !!selectedDate;
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate ? dayjs(selectedDate).format('DD [de] MMMM') : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -20,7 +24,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            Terça-Feira <span>03 de Novembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
           <TimePickerList>
             <TimePickerItem>01:00</TimePickerItem>
